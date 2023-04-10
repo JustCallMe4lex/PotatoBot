9c1ff14bb249c02ad86ad61a6ebe45f38530f54f
Alexander Rain Tamayo <xanderain0630@gmail.com>
2023-04-10 13:00:27 +0800

    logs changes

--- cogs/logs.py
+++ cogs/logs.py
@@ -8,7 +8,7 @@ class Logs(commands.Cog):
         self.client = client
 
     @commands.Cog.listener()
-    async def on_member_join(self, member):
+    async def on_member_join(self, member): # Member leaves the server
         with open("cogs/jsonfiles/logs.json", "r") as f:
             logs = json.load(f)
 
@@ -18,11 +18,12 @@ class Logs(commands.Cog):
                               description="A new challenger approaches!",
                               color=discord.Color.blue())
         embed.add_field(name="User:", value=member.mention, inline=False)
+        embed.set_thumbnail(url=member.avatar)
 
         await log_channel.send(embed=embed)
 
     @commands.Cog.listener()
-    async def on_member_remove(self, member):
+    async def on_member_remove(self, member): # Member leaves the server
         with open("cogs/jsonfiles/logs.json", "r") as f:
             logs = json.load(f)
 
@@ -32,8 +33,43 @@ class Logs(commands.Cog):
                               description="A user left da hood!",
                               color=discord.Color.blue())
         embed.add_field(name="User:", value=member.mention, inline=False)
+        embed.add_field(url=member.avatar)
 
         await log_channel.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after): # Member edits message
+        with open("cogs/jsonfiles/logs.json", "r") as f:
+            logs = json.load(f)
+
+        log_channel = discord.utils.get(after.author.guild.channels, name=logs[str(after.author.guild.id)])
+
+        if not after.author.bot:
+            if before.content != after.content:
+                embed = discord.Embed(title="Message Edited!",
+                                      description=f"{after.author.display_name} has been editing messages.",
+                                      color=discord.Color.blue())
+                embed.add_field(name="Message Before:", value=before.content, inline=False)
+                embed.add_field(name="Message After:", value=after.content, inline=False)
+                embed.set_thumbnail(url=after.author.avatar)
+
+                await log_channel.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):  # Member deletes message
+        with open("cogs/jsonfiles/logs.json", "r") as f:
+            logs = json.load(f)
+
+        log_channel = discord.utils.get(message.author.guild.channels, name=logs[str(message.author.guild.id)])
+
+        if not message.author.bot:
+            embed = discord.Embed(title="Message Deleted!",
+                                  description=f"{message.author.display_name} has been deleting messages.",
+                                  color=discord.Color.blue())
+            embed.add_field(name="Message Deleted:", value=message.content, inline=False)
+            embed.set_thumbnail(url=message.author.avatar)
+
+            await log_channel.send(embed=embed)
 
 async def setup(client):
     await client.add_cog(Logs(client))
