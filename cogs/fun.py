dba0cb0170f341df99aeda46b436a256abe192b0
Alexander Rain Tamayo <xanderain0630@gmail.com>
2023-04-11 15:30:23 +0800

    new fun commands

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime
 
 import discord
 from discord.ext import commands
@@ -29,6 +30,52 @@ class Fun(commands.Cog):
             embed = discord.Embed(title="Command Failed!", color=discord.Color.red())
             embed.add_field(name="Dice Command Failed!",
                             value="Please enter the dice amount. (eg. 1d6)",
+                            inline=False)
+            embed.set_thumbnail(url="https://www.iconsdb.com/icons/preview/red/x-mark-3-xxl.png")
+
+            await ctx.send(embed=embed)
+
+    @commands.command()
+    async def magic8ball(self, ctx, *, question: str):
+        """The magic 8-ball"""
+        responses = ["It is certain.",
+                     "It is decidedly so.",
+                     "Without a doubt.",
+                     "Yes definitely.",
+                     "You may rely on it.",
+                     "As I see it, yes.",
+                     "Most likely.",
+                     "Outlook good.",
+                     "Yes.",
+                     "Signs point to yes.",
+                     "Reply hazy, try again.",
+                     "Ask again later.",
+                     "Better not tell you now.",
+                     "Cannot predict now.",
+                     "Concentrate and ask again.",
+                     "Don't count on it.",
+                     "My reply is no.",
+                     "My sources say no.",
+                     "Outlook not so good.",
+                     "Very doubtful."]
+        answer = random.choice(responses)
+
+        embed = discord.Embed(title="Magic 8 Ball!",
+                              description="Ask and it will answer.",
+                              color=discord.Color.random(),
+                              timestamp=datetime.utcnow())
+        embed.add_field(name=f"{ctx.author.name}'s Question:", value=question, inline=False)
+        embed.add_field(name="Answer:", value=answer, inline=False)
+        embed.set_thumbnail(url="https://www.horoscope.com/images-US/games/game-magic-8-ball-no-text.png")
+
+        await ctx.send(embed=embed)
+
+    @magic8ball.error
+    async def magic8ball_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(title="Command Failed!", color=discord.Color.red())
+            embed.add_field(name="8 Ball Command Failed!",
+                            value="Please enter your question.",
                             inline=False)
             embed.set_thumbnail(url="https://www.iconsdb.com/icons/preview/red/x-mark-3-xxl.png")
 
